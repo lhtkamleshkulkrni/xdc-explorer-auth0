9fa174c8622633f731598c5774531fda530bd591
kajal_KajalBobal <kajal@leewayhertz.com>
2022-02-01 11:03:29 +0530

    globalid apis

--- app/modules/user/blManager.js
+++ app/modules/user/blManager.js
@@ -20,6 +20,8 @@ import Config from '../../../config';
 import HttpService from "../../service/http-service";
 import * as globalidCrypto from 'globalid-crypto-library';
 import fs from "fs";
+import * as jwt from 'jsonwebtoken'
+import {filter, isEmpty, flattenDeep, map} from 'lodash'
 
 const hash = require("object-hash");
 export default class BlManager {
@@ -158,63 +160,143 @@ export default class BlManager {
         return UserCookiesSchema.findAndUpdateData({userId: requestData.userId}, {cookiesAllowed:requestData.cookiesAllowed});
     }
 
-    globalIdUserInfo = async(requestData)=>{
-        if (!requestData || !requestData.token)
+    globalIdUserInfo = async (requestData) => {
+        if (!requestData || !requestData.id_token || !requestData.accessToken)
             throw Utils.error({}, apiFailureMessage.INVALID_PARAMS, httpConstants.RESPONSE_CODES.FORBIDDEN);
-        let accessAndIdToken=JSON.parse(await this.accessAndIdToken(requestData.token));
-        return accessAndIdToken.id_token;
-        // console.log("accessAndIdToken",accessAndIdToken);
-        // console.log("accessAndIdToken",accessAndIdToken.id_token);
-        let piiInfo=await this.claimTokenUsingIdToken(accessAndIdToken.id_token);
+        let claims = await this.decryptClainTokens(requestData.id_token, requestData.access_token);
+        return claims;
     }
 
-    async accessAndIdToken(codes){
-        let URL=Config.GLOBAL_ID_API_URL+"auth/token";
-        let headers={
+    getGlobalIdTokens= async (requestData) => {
+        if (!requestData || !requestData.code)
+            throw Utils.error({}, apiFailureMessage.INVALID_PARAMS, httpConstants.RESPONSE_CODES.FORBIDDEN);
+        let accessAndIdToken=JSON.parse(await this.accessAndIdToken(requestData.code));
+          if(!accessAndIdToken || !accessAndIdToken.id_token || !accessAndIdToken.access_token){
+            throw Utils.error({}, apiFailureMessage.INVALID_SESSION_TOKEN, httpConstants.RESPONSE_CODES.FORBIDDEN);
+        }
+        return accessAndIdToken;
+    }
+
+    async accessAndIdToken(codes) {
+        let URL = Config.GLOBAL_ID_API_URL + "auth/token";
+        let headers = {
             'Content-Type': 'application/json'
         };
-        let data={
+        let data = {
             "client_id": Config.GLOBAL_ID_CLIENT_ID,
             "client_secret": Config.GLOBAL_ID_CLIENT_SECRET,
             "grant_type": "authorization_code",
             "refresh_token": "string",
-            "code":codes,
+            "code": codes,
             "redirect_uri": Config.GLOBAL_ID_REDIRECT_URL
         }
         return await HttpService.executeHTTPRequest(httpConstants.METHOD_TYPE.POST, URL, "", JSON.stringify(data), headers);
     }
 
-    async claimTokenUsingIdToken(idToken){
-        idToken="accessAndIdToken eyJhbGciOiJSUzI1NiIsImtpZCI6InB1YmxpYzpkMGM2M2RhNi1lNzkzLTRhNTctYWI3MC1kMmY4MTFlZjA1MzYifQ.eyJhdF9oYXNoIjoiVUpwdk1EeEtUZS1ON2NtTEVsU1k2ZyIsImF0dGVzdGF0aW9ucyI6WyI5NGMyYzA0Ni1jYmMxLTQ1N2QtYmQwOC0wNTYwOWIzMzMzZGUiXSwiYXVkIjpbIjgwOGU3OTFhLTcwYjQtNDNhNC1iYjMwLTZmMzNjNjEwZDRlYyJdLCJhdXRoX3RpbWUiOjE2NDMyNzQ2MzgsImV4cCI6MTY0MzI4MTg1NiwiaWF0IjoxNjQzMjc0NjU2LCJpZHAuZ2xvYmFsaWQubmV0L2NsYWltcy8zNWZkZTMyNC03NzM2LTQ5MWEtYjg5Zi1jMjk4NTQ0MTczMDAiOnsiMjBjNjE0YjQtMzRkNy00N2I1LWI2YWYtMWM5NGFlZmJhOTQxIjpbXSwiMjFmMWM2NGUtNWRmNS00YzI0LTg5OTYtNGVlMDhkYTEwNDA5IjpbXSwiMjhjNTU3YTMtMjM1OC00YzU5LWJhM2EtYzQxNTA5MTlmNjgwIjpbXSwiM2ViMGVkMTMtMjY5NS00YjhlLThhNTAtM2IwOGQyZTgwNTljIjpbXSwiNDA5MjNiOWUtMTQ2Ny00MTNlLWE5M2EtNTE0MWUyOWYwYWY0IjpbXSwiNGZiMmFhNzQtNmMwYi00ZTRhLTg2Y2ItODlmYmVlNTRlYTIwIjpbXSwiNTY2ZTQ0MDctYTMzNi00MTYyLWJlOGYtNjFmNjAyMjFiYjVmIjpbXSwiNmUwODY4MmYtZjYwNy00MDc0LWJjMTctY2JjYWI3OTNmYWNlIjpbXSwiODFlY2VkNTEtODRiMC00YzJkLWE1ZDgtYTJlMjk5NDk5Y2QzIjpbImhzUk9zR2VJSG5oeFIxbTF5QzhPOVVUV3lMSllqdkd1ZzhFNC82VmtCM2hIMnpsazRYcEMzT3hpNUt1Ti9FSVMxQUlreTlUNTN3dzI2aGc2Mk5nRFNpOExVaU5aYjBaeUFxeVhzemV2cUpCKzVabDVJelEybnJ1OXB4d2JWLzlxYjh3NkRUVVhKcnZSOTVLS2k1WEdyK213cHI5ZWtTY1MrMWNxM0F2czhmeDZXVUdyRTNnT0dMaHJxZlovai9MN0J3N2xvZ1hmd3FEN2hCTmd1ekRZcFdOeFdnUHRJREVsTitPcGNwcUZIV1lZRDdSZFowQUZoWlFFRUVTWldva3VKNWVjL3pTSk1Pa2NJLzFVemVpaGVBZEhmOTRFcHNLZXN2QnRFWnJYc3pNdjNHVHJQZXZ3ckI1Ykw4VlAvYWVDOXBmeWRITWxkeG9uU1dOSjlTTVZ0enJ5S2FLNE5ZenJwdUliU1hjR2h3TGdVZ1JkTXcrUkJLUVpqUnBvdjVJZVkzUXBMQUV1WE80clBQQU5MZWc0a3VhOU8zSi9tRkJXdnY1RG9MM2xCclBtRkxYR1V0OTlLTU1xSmZjRCsrRnM2ZDVGU1hkZDVaK01IdGVFc3J0YkJpQTFsRXVnaC96UWo2b0hsblp0alVTaGdpcmdNbzcvemtEeTNKQnlYYjl6NDJDbHFuN3FBSDNyZ1hvYTlRV0Nyc3d5eGMyMjNGVDFzcXhkMTRvWXkzYVNNZ2d3Ri9aM3dCRkdrNGY5Zy96c3ZIaGZqRHd1c3Ruekd0b1ZUWGZ1ZmtCSHcwdnd0aFMxYmRlN3JicTZpVVRTV09hSU1UNFdScnNoS210eUNSMTVwSXdnRXpubHRUM0RJTS9mRE1OckJkUHdIZXA3RTVLT2QxNW9kQTRUbHpzPSJdLCI4Yzg4ZmQ4Yy00ODg2LTQzZGItODY4Ny1lZmExYjk0YTY1MTYiOltdLCJiMGFhZWM1Yy1jNjk4LTRhNDctYTg4Mi0wODA4NDA0NzdjNDUiOltdLCJiMjMwZTM0OS1kNjk5LTQ5ODAtYjljNS0yMzUwYmY3ZGU2YTgiOltdLCJjOWQwZjBjNi04NWE4LTQxZGQtODJhZi01OWJjNGZmZGY0NjciOltdLCJmZGRjZDlhZC05OWViLTRkNzktOWRmNi00ZGVlYzMyZGVlZDIiOltdfSwiaWRwLmdsb2JhbGlkLm5ldC9jbGFpbXMvYjM0NzJkNDEtNzYyMC00ZTFjLThlNjItNjJkNTczNDc0MmNmIjp7IjAzM2JiOGFkLWQ1YmUtNDE1Ni1hMDMyLTliNzFhYTUyNjdkOSI6W10sIjA2MjdmN2RkLTRjMTEtNDk2Ny04MWRlLWZhMGRlNTA4NDU3MiI6W10sIjA4OTk4Njk1LWZjZWYtNDQ4Yi1hMTZiLTlhNjY3Y2VmYjZkOCI6W10sIjQzM2I0MmE3LTM3YWYtNDNkZi05YTM4LThmY2RmYTNjNzdlNyI6W10sIjQ5OTdkNDVhLTllNjQtNGJjOC1iYzdiLTNlMTUyNjJiZGQ3ZSI6W10sIjRhYmRiNjU1LWNiZGQtNDM5NS05ZjdiLTNhYTRiMGQ3N2YzYiI6W10sIjVkMmUzZjUyLTg0NjUtNDY5MS04OTA0LWU5NjY1ZWZkMjI5ZSI6W10sImI0MzVmYWJlLTIyMmMtNGY2Zi05NTc1LTAwZTI5MmVhZDE2MCI6W10sImRmMTgwZjIzLTk5M2UtNGU0Yy1hZTQxLTdlZjIwYzU2ZTc1OCI6W10sImZkN2I3MWE4LWIzNzAtNDJlZC04Mjk2LTY2ZTViMmE3NzdkNyI6W119LCJpZHAuZ2xvYmFsaWQubmV0L2NsYWltcy9iNmY0N2IxMC05ZjY4LTQwOTMtYjdlOC02Yzc0NjI4MzMxNjkiOnsiZGE0Y2U5NDMtZTM2Yy00MmJkLTgzNzktOTVhYzE2ZTBhOGE5IjpbXX0sImlzcyI6Imh0dHBzOi8vZ2xvYmFsLmlkLyIsImp0aSI6IjBiOTgyMzRmLWVmYzAtNDExYy1iZTJjLTNlNzg5NDZlNzhkNSIsIm5vbmNlIjoiMTIzNDU2NzgiLCJyYXQiOjE2NDMyNzQ2MzgsInNpZCI6ImY2NmYzMjQ5LWZiMzEtNDg1OC04ZjUyLTc5NTNkMjhiYzFmNyIsInN1YiI6ImI5MjFkMDI0LTUzZmEtNDE4ZS1hODljLWI0MTE1MDY3NDhlNCJ9.r6bIw5FXsEIKPMwxp3MmCiVyj6k9q6oBtSb-K8JLtDb9uQ4BY2dHVQwFczdIPq1FJ_HsQNJCufHCvlY0KQrhvz2vpseqM5H2mt4BCDhUWRUMOiMurfo5wHj8E8K17_hzUIc8OWOzX_lJ62Jcb2o9EGbybCDtzAMF51yeuWyfcHodOLXuogShfhyTSZINTdUkkDED77ffZM2HZt27P5iByu-54AhEymTtUVXEoiOLb2A_cY-3nGOoXRSgpnyNm2rLFfXwIErovFx2vFZiXv7S59HM488XFMcKuWEASPQXGpLY5OCUiFxC4tod-fyKamcBZtOkFv7U9-VJNxyqBGEYC6BnN356QBu369HqHqkeE4HU3NfV9FC4nA5jNfI7ZnzAUSJWXlDijF37C-rt9NtBYzzGUEtzVOf0VU3mRrd_qSEiFvct0TrN5kZF0EOdx-2-9oVGCZXJXnGdw9Ht0coR4SU5dKH_mmGvswyCqe0jRDn7gn0JrHdFmPPJTJpSExzxqf0uVvK8ruC_a2C8z4cJ438IU9mDtMva8ceAUs6vAoN_PKWv3D30ThfLgoaMcTS0Ld9Kk1KWLuzpVXb0CPF_ZN82r8uMzezOS98kqDGCM_Rbuf7Puk3-STpNIqdMy42m2NYWCYxNO8RmOboiUgWdW-3egrUCSRIjSf8WfSfuhPs";
-        try{
-            let password=fs.readFileSync(__dirname+"/key_for_GlobaliD.key")
+
+   
+    async getTokensForThePII(id_token) {
+        const decoded_token = jwt.decode(id_token);
+        let consents = await _.filter(decoded_token, (value, index) => {
+            return _.includes(index, 'idp.globalid.net/claims/') && _.isEmpty(value) !== true
+        });
+        console.log("consents", consents);
+        let response;
+        try {
+            response = _.flattenDeep(await Promise.all(consents.map(this.decryptedConsent)));
+            console.log("response", response);
+        } catch (err) {
+            console.log("err", err);
+        }
+        return response;
+    }
+
+    async decryptedConsent(consent) {
+        console.log("consent", consent);
+        // let privates=process.env.PRIVATE_KEY;
+        return Promise.all(
+            _.map(consent, async (values) =>
+                Promise.all(
+                    _.map(values, async (value) =>
+                        gidCrypto.RSA.decrypt(Config.PRIVATE_KEY, value),
+                    ),
+                ),
+            ),
+        )
+    }
+
+
+    async claimTokenUsingIdToken(idToken) {
+        idToken = "REMOVED";
+        try {
+            let password = fs.readFileSync(__dirname + "/private.key")
             const result = globalidCrypto.RSA.decrypt(password.toString('utf8'), "observer");
-            // const tokenData = globalidCrypto.AES.decrypt(result, result);
-
-            // const result = globalidCrypto.AES.decrypt(idToken, password);
-        console.log("result",result);}
-        catch(err){
-            console.log("err",err);
+            console.log("result", result);
+        } catch (err) {
+            console.log("err", err);
         }
     }
 
-    async getPIIData(codes){
-        let URL=Config.GLOBAL_ID_API_URL+"auth/token";
-        let headers={
-            'Content-Type': 'application/json'
+
+    async decryptClainTokens(idToken, accessToken) {
+        const decoded_token = jwt.decode(idToken)
+        const consents = (filter(decoded_token, (value, index) => index.includes('idp.globalid.net/claims/') && !isEmpty(value)))
+        const result = [];
+        for(let index=0; index< consents.length; index++){
+            const res = await this.decryptPrivateDataTokenPerConsent(consents[index], accessToken);
+            result.push(res);
+        }
+        return result;
+    }
+
+    async decryptPrivateDataTokenPerConsent(consent, accessToken) {
+        let data = await this.decryptedConsent(consent);
+        data = data.filter(arr => {
+            if (arr.length)
+                return arr;
+        })
+
+        let result = [];
+        for (let index = 0; index < data.length; index++) {
+            const obj = await this.getPIIData(data[index], accessToken)
+            result.push(JSON.parse(obj));
+        }
+        result = result[0];
+        const response = []
+        for (let index = 0; index < result.length; index++) {
+            let password = gidCrypto.RSA.decrypt(Config.PRIVATE_KEY, result[index].encrypted_data_password);
+            const tokenData = gidCrypto.AES.decrypt(result[index].encrypted_data, password);
+            response.push(JSON.parse(tokenData));
+        }
+        return response;
+    }
+
+    async decryptedConsent(consent) {
+        return Promise.all(
+            _.map(consent, async (values) =>
+                Promise.all(
+                    _.map(values, async (value) =>
+                        gidCrypto.RSA.decrypt(Config.PRIVATE_KEY, value)
+                    ),
+                ),
+            ),
+        )
+    }
+
+
+    async getPIIData(codes, accessToken) {
+        let URL = Config.GLOBAL_ID_API_URL + "vault/get-encrypted-data";
+        let headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
         };
-        let data={
-            "client_id": Config.GLOBAL_ID_CLIENT_ID,
-            "client_secret": Config.GLOBAL_ID_CLIENT_SECRET,
-            "grant_type": "authorization_code",
-            "refresh_token": "string",
-            "code":codes,
-            "redirect_uri": Config.GLOBAL_ID_REDIRECT_URL
+        let data = {
+            private_data_tokens: codes
         }
-        return await HttpService.executeHTTPRequest(httpConstants.METHOD_TYPE.GET, URL, "", data, headers);
+        return await HttpService.executeHTTPRequest(httpConstants.METHOD_TYPE.POST, URL, "", data, headers);
     }
 
-    async decryptPIIInfo(){
+    async decryptPIIInfo() {
 
     }
 }
