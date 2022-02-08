969c82c779eb1e997c44e044407e9b7f1d7ce233
AdityaDubey1811 <adityad@leewayhertz.com>
2022-02-08 20:23:40 +0530

    globalid clientID updated

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
-import APP from 'express'
-import DBConnection from './config/dbConnection'
-import Utils from './app/utils'
-import Config from './config'
-import routes from './routes'
+import APP from "express";
+import DBConnection from "./config/dbConnection";
+import Utils from "./app/utils";
+import Config from "./config";
+import routes from "./routes";
 import AMQP from "./library";
 import QueueController from "./app/modules/queue";
-import { httpConstants } from './app/common/constants'
+import { httpConstants } from "./app/common/constants";
 
-const app = new APP()
-require('./config/express')(app)
-global.lhtWebLog = Utils.lhtLog
- 
-class Server {  
-  static listen () {
-    Promise.all([DBConnection.connect()],AMQP.conn(Config.AMQP_HOST_URL, true)).then(() => {
-      app.listen(Config.PORT)
-      Utils.lhtLog('listen', `Server Started on port ${Config.PORT}`, {}, 'AyushK', httpConstants.LOG_LEVEL_TYPE.INFO)
-      routes(app)
-      require('./config/jobInitializer')
-      // new QueueController().initializeRabbitMQListener();
-    }).catch(error => Utils.lhtLog('listen', 'failed to connect', { err: error }, 'AyushK', httpConstants.LOG_LEVEL_TYPE.ERROR))
+const app = new APP();
+require("./config/express")(app);
+global.lhtWebLog = Utils.lhtLog;
+
+class Server {
+  static listen() {
+    Promise.all([DBConnection.connect()], AMQP.conn(Config.AMQP_HOST_URL, true))
+      .then(() => {
+        app.listen(Config.PORT);
+        Utils.lhtLog(
+          "listen",
+          `Server Started on port ${Config.PORT}`,
+          {},
+          "AyushK",
+          httpConstants.LOG_LEVEL_TYPE.INFO
+        );
+        routes(app);
+        require("./config/jobInitializer");
+        // new QueueController().initializeRabbitMQListener();
+      })
+      .catch((error) =>
+        Utils.lhtLog(
+          "listen",
+          "failed to connect",
+          { err: error },
+          "AyushK",
+          httpConstants.LOG_LEVEL_TYPE.ERROR
+        )
+      );
   }
 }
 
-Server.listen()
+Server.listen();
